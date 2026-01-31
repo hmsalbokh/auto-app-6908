fd01bfbc948801573b0ab60ea60aa06604f0f841
hmsalbokh <114269457+hmsalbokh@users.noreply.github.com>
2026-01-31 05:09:32 +0300

    deploy

---
+++ tailwind.config.ts
@@ -0,0 +1,1 @@
+import type { Config } from 'tailwindcss'; export default { content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'], theme: { extend: { colors: { primary: '#6366f1', secondary: '#f43f5e' } } }, plugins: [] } as Config;
